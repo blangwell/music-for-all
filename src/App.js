0e3f243f57bdf920967e1626ff638af4c1db499c
blangwell <b.langwell@outlook.com>
2020-10-30 11:35:43 -0700

    [App] Add Components/About.js && /GetInvolved.js, add Router and Links to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
 
+import Home from './Components/Home';
+import About from './Components/About';
+import GetInvolved from './Components/GetInvolved';
+
 function App() {
   return (
-    <div className="App">
-      {/* <Button>Test Button</Button> */}
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <Link to="/">Home</Link>{' '}
+          <Link to="/about">About</Link>{' '}
+          <Link to="/getinvolved">Get Involved</Link>{' '}
+        </nav>
+        {/* <Route exact path="/" comonent={Home}/> */}
+        <Route exact path="/" render={() => <Home/>}/>
+        <Route path="/about" component={About}/>
+        <Route path="/getinvolved" component={GetInvolved}/>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/GetInvolved.js
@@ -0,0 +1,7 @@
+function GetInvolved() {
+  return(
+    <h1>Get Involved</h1>
+  )
+}
+
+export default GetInvolved;
