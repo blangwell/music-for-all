cd7ec42c75c5152be153ae35bc4594e13650087f
blangwell <b.langwell@outlook.com>
2020-10-30 13:35:51 -0700

    [navbar] Add BootstrapNavbar links

--- src/App.js
+++ src/App.js
@@ -12,23 +12,27 @@ import Navbar from 'react-bootstrap/Navbar';
 import Home from './Components/Home';
 import About from './Components/About';
 import GetInvolved from './Components/GetInvolved';
+import BootstrapNavbar from './Components/BootstrapNavbar';
+
 import { NavbarBrand } from 'react-bootstrap';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <nav>
-          <Link to="/">Home</Link>{' '}
-          <Link to="/about">About</Link>{' '}
-          <Link to="/getinvolved">Get Involved</Link>{' '}
-        </nav>
-        {/* <Route exact path="/" comonent={Home}/> */}
-        <Route exact path="/" render={() => <Home/>}/>
-        <Route path="/about" component={About}/>
-        <Route path="/getinvolved" component={GetInvolved}/>
-      </div>
-    </Router>
+    <BootstrapNavbar />
+    // <Router>
+
+    //   <div className="App">
+    //     <nav>
+    //       <Link to="/">Home</Link>{' '}
+    //       <Link to="/about">About</Link>{' '}
+    //       <Link to="/getinvolved">Get Involved</Link>{' '}
+    //     </nav>
+    //     {/* <Route exact path="/" comonent={Home}/> */}
+    //     <Route exact path="/" render={() => <Home/>}/>
+    //     <Route path="/about" component={About}/>
+    //     <Route path="/getinvolved" component={GetInvolved}/>
+    //   </div>
+    // </Router>
   );
 }
 
--- src/Components/BootstrapNavbar.js
+++ src/Components/BootstrapNavbar.js
@@ -2,16 +2,14 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  // Link
 } from 'react-router-dom';
 
-import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
 
-import Home from './Components/Home';
-import About from './Components/About';
-import GetInvolved from './Components/GetInvolved';
-import { Nav, NavbarBrand } from 'react-bootstrap';
+import Home from './Home';
+import About from './About';
+import GetInvolved from './GetInvolved';
+import { Nav } from 'react-bootstrap';
 
 function BootstrapNavbar() {
   return (
@@ -23,15 +21,31 @@ function BootstrapNavbar() {
                 <Navbar.Brand href="#home">Music 4 All</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav classname="mr-auto">
+                  <Nav className="mr-auto">
                   <Nav.Link href="/">Home</Nav.Link>
                   <Nav.Link href="/about">About</Nav.Link>
+                  <Nav.Link href="/getinvolved">Get Involved</Nav.Link>
                   </Nav>
                 </Navbar.Collapse>
               </Navbar>
+              
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route path="/about">
+                  <About />
+                </Route>
+                <Route path="/getinvolved">
+                  <GetInvolved />
+                </Route>
+              </Switch>
+
             </Router>
           </div>
         </div>
       </div>
   )
-}
+};
+
+export default BootstrapNavbar;
