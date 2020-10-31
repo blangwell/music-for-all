8186884ae9c990f32864f66d05c671f7b1223a28
blangwell <b.langwell@outlook.com>
2020-10-31 16:57:33 -0700

    [copy] Add copy to About.js and GetInvolved.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,23 @@
 function About() {
 
   return(
+    <div id="about">
     <h1>About</h1>
+      <p>
+      Cindy Van Horn has been bringing her love of music to students of all ages
+  for over 35 years. She directs children’s choirs, teaches class and private
+  voice lessons, and directs a special education enrichment program in the
+  public school system. This special education program has become her
+  major focus in the last five years. She wants to build a program where
+  people of all ages with special needs can access the joy of music
+  experience and performance. Music 4 All is that program.
+  Music 4 All is a program designed to teach music appreciation and
+  education through activities tailored to people with special needs. All
+  musicians ages 5 to 95 are encouraged to participate. There is a Music 4
+  All program for all levels of ability and experience.
+    </p>
+
+    </div>
   )
   
 }
--- src/Components/GetInvolved.js
+++ src/Components/GetInvolved.js
@@ -1,6 +1,28 @@
 function GetInvolved() {
   return(
-    <h1>Get Involved</h1>
+    <div id="get-involved">
+      <h1>Get Involved</h1>
+      <h2>Choir</h2>
+      <p>If you are new to Music 4 All, we suggest you start with one of the CHOIR
+programs. There is one for elementary, one for secondary and one adult
+group for those folks who have completed school. These are weekly, 30-
+minute group zoom meetings that feature voice building exercises, song
+preparation and creative movement. It’s a high-energy time of singing
+exploration. CHOIR programs are an economical way to start, at $10 per
+student per month.</p>
+      <h2>Group Instruction</h2>
+<p>Class voice instruction is for the student who wants more individualized
+attention. Typically five students, each class concentrates on vocal
+exercises and song preparation. Students of all abilities and levels of
+verbal expression will be accommodated. Classes meet on Zoom weekly
+for 30 minutes and are $40 per month.</p>
+      <h2>Private Instruction</h2>
+<p>For the serious students, we offer private voice lessons conducted through
+Zoom. These are more intense, and require outside practice time to
+progress. Private lessons are weekly, 30-minute sessions and cost $80 per
+month.</p>
+
+    </div>
   )
 }
 
