95b983b96aef79d39fa93acb3838acbb768d62a7
blangwell <b.langwell@outlook.com>
2020-11-01 19:13:36 -0800

    [contact] Add contact link and route to BootstrapNavbar.js and add form to Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,15 +1,29 @@
+import { Form, Button } from 'react-bootstrap'
+
 function Contact() {
 
   return(
-    <div>
-      <h1 className="page-title">Contact</h1>
-      <form>
-        <label htmlFor="email">Email Address</label>
-        <input id="email" type="email" name="email" />
+    // <div>
+    //   <h1 className="page-title">Contact</h1>
+    //   <form>
+    //     <label htmlFor="email">Email Address</label>
+    //     <input id="email" type="email" name="email" />
+    //     <label htmlFor="message">Message</label>
+    //     <textarea id="message" type="text" name="message" />
 
-
-      </form>
-    </div>
+    //   </form>
+    // </div>
+    <Form>
+      <Form.Group controlId="email-address" className="w-25">
+        <Form.Label>Email Address</Form.Label>
+        <Form.Control type="email" placeholder="Your email" />
+      </Form.Group>
+      <Form.Group controlId="email-message" className="w-50">
+        <Form.Label>Your Message</Form.Label>
+        <Form.Control as="textarea" rows={5} />
+      </Form.Group>
+      <Button variant="primary">Submit</Button>
+    </Form>
   )
 }
 
