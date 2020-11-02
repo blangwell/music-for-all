0cfc70bcab121eb92e40f746376ea8d14fe72551
blangwell <b.langwell@outlook.com>
2020-11-01 18:46:39 -0800

    [form] Install react-hook-form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,14 @@
 function Contact() {
+
   return(
     <div>
       <h1 className="page-title">Contact</h1>
+      <form>
+        <label htmlFor="email">Email Address</label>
+        <input id="email" type="email" name="email" />
+
+
+      </form>
     </div>
   )
 }
