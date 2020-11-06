68e86e0f118cffd87f780feaa58286e3dcab9432
blangwell <b.langwell@outlook.com>
2020-11-06 10:16:28 -0800

    [landing] Add copy and /getinvolved link to Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,14 @@
 import LandingBanner from './LandingBanner';
+import {Link} from 'react-router-dom'
 
 function Home(props) {
   return (
     <div className="d-flex flex-column" id="home">
-      <h1 className="page-title">Home</h1>
       <LandingBanner />
+      <p className="page-copy m-2">Music 4 All is a program designed to teach music appreciation and
+education through activities tailored to people with special needs. All
+musicians ages 5 to 95 are encouraged to participate. There is a Music 4
+All program for all levels of ability and experience. <Link to="/getinvolved">Get involved!</Link> </p>
     </div>
   )
 }
