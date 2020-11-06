16e9c2ed709b901cea192ae7a30a3730e761abab
blangwell <b.langwell@outlook.com>
2020-11-06 09:28:38 -0800

    [landingbanner] Add puzzle-piece.png img to LandingBanner.js and style

--- src/Components/LandingBanner.js
+++ src/Components/LandingBanner.js
@@ -1,8 +1,11 @@
+import puzzlePiece from '../puzzle-piece.png'
+
 function LandingBanner() {
   return (
-    <div>
-      <h1><span id="miss-cindy">Miss Cindy's
+    <div id="landing-banner">
+      <h1><span id="miss-cindy">Miss Cindy's<br />
       </span> <span id="m4a">Music 4 All</span></h1>
+      <img src={puzzlePiece} alt="puzzle piece" id="puzzle-piece"/>
     </div>
   )
 }
