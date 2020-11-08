3215e8eb59750296adfaa75363b1c590fd28ec97
blangwell <b.langwell@outlook.com>
2020-11-08 13:12:23 -0800

    [contact] Add axios call to handleSubmit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,13 +1,23 @@
-import { useState } from 'react'
-import { Form, Button } from 'react-bootstrap'
+import { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+const SERVER_URL = process.env.SERVER_URL
 
 const Contact = () => {
-  const [email, setEmail] = useState('')
-  const [message, setMessage] = useState('')
+  let [email, setEmail] = useState('')
+  let [message, setMessage] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
+    const sentMessage = { email, message }
+    axios.post(`http://localhost:8080/sendemail`, sentMessage)
+    .then(response => {
+      console.log(response);
+      setEmail('');
+      setMessage('');
+    })
+    .catch(err => console.log(err))
+    // console.log(e);
   }
 
   return(
@@ -15,13 +25,13 @@ const Contact = () => {
       <h1 className="page-title">Contact</h1>
       {/* <Form onSubmit={handleSubmit}> */}
       <form onSubmit={handleSubmit}>
-        <Form.Group controlId="email-address" className="w-25">
+        <Form.Group controlId="email" className="w-25">
           <Form.Label>Email Address</Form.Label>
-          <Form.Control type="email" placeholder="Your email" onChange={(e) => setEmail(e.target.value)} required/>
+          <Form.Control type="email" name="email" value={email} placeholder="Your email" onChange={(e) => setEmail(e.target.value)} required/>
         </Form.Group>
-        <Form.Group controlId="email-message" className="w-50">
+        <Form.Group controlId="message" className="w-50">
           <Form.Label>Your Message</Form.Label>
-          <Form.Control as="textarea" rows={5} onChange={(e) => setMessage(e.target.value)} required/>
+          <Form.Control as="textarea" name="message" value={message} rows={5} onChange={(e) => setMessage(e.target.value)} required/>
         </Form.Group>
         <Button type="submit" variant="primary">Submit</Button>
       </form>
